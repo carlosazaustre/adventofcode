3a67fd9d850b525b1312ed872cee1b26731af3a0
Carlos Azaustre <cazaustre@gmail.com>
2018-12-02 14:21:35 +0100

    day 1 part 2

--- day_01.js
+++ day_01.js
@@ -1,15 +1,30 @@
 const fs = require('fs')
-const readline = require('readline')
 
-const rl = readline.createInterface({
-  input: fs.createReadStream('./day_01_input.txt')
-})
+const data = fs.readFileSync('./day_01_input.txt', { encoding: 'UTF-8' })
+const frequencies = data.split(/\n/g).map(Number)
 
+/* PART 1 */
+
+// Array Reduce Docs:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+const finalFreq = frequencies.reduce((acum, current) => acum + current)
+console.log('Final Frequency ', finalFreq)
+
+/* PART 2 */
+
+// Set object Docs:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+const freqReached = new Set()
 let frequency = 0
 
-// Part 1
-rl.on('line', num => frequency += Number(num))
-rl.on('close', () => console.log(`Final: ${frequency}`))
-
-// Part 2
+while(true) {
+  for (let currentFreq of frequencies) {
+    freqReached.add(frequency)
+    frequency += currentFreq
+    if (freqReached.has(frequency)) {
+      console.log('Reached Twice: ', frequency)
+      return
+    }
+  }
+}
 
