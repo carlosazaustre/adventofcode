5b687f4dbde2a1460bd6496dee6119196a8bec15
Carlos Azaustre <cazaustre@gmail.com>
2018-12-02 19:23:09 +0100

    re-refactor day 2

--- day_02/index.js
+++ day_02/index.js
@@ -7,43 +7,42 @@
 
 const { getData } = require('../utils')
 
-function getNumberOfTimes (array, value) {
-  let times = 0
-  array.forEach(v => v === value && times++)
-  return times
+
+/* PART 1 */
+
+function getValueFromID (acum, current) {
+  // Convert current string into a char array
+  const letters = [...current]
+  
+  let numberOfTimes = {}
+  
+  // Store each letter as key in an object.
+  // value will be the number of occurrences/times that
+  // appears.
+  letters.forEach(letter => {
+    numberOfTimes[letter] = numberOfTimes[letter] 
+      ? numberOfTimes[letter] + 1
+      : 1
+  })
+
+  // Array.prototype.some
+  // The some() method tests whether at least one
+  // element in the array passes the test implemented 
+  // by the provided function.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+  if (Object.keys(numberOfTimes).some(key => numberOfTimes[key] === 2)) acum[0]++
+  if (Object.keys(numberOfTimes).some(key => numberOfTimes[key] === 3)) acum[1]++
+  
+  return acum
 }
 
-function getChecksum (arrayData) {
+function getChecksum (data) {
   // Array Reduce doc:
   // The reduce() method executes a reducer function 
   // (that you provide) on each member of the array 
   // resulting in a single output value.
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-  const checksum = arrayData.reduce((acum, current) => {
-    // Convert current string ID into a char array
-    const letters = [...current]
-
-    let numberOfTimes = {}
-
-    // Store each letter as key in an object.
-    // value will be the number of occurrences/times that
-    // appears.
-    letters.forEach(letter => {
-      numberOfTimes[letter] = numberOfTimes[letter] 
-        ? numberOfTimes[letter]++
-        : 1
-    })
-    
-    // Array.prototype.some
-    // The some() method tests whether at least one
-    // element in the array passes the test implemented 
-    // by the provided function.
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
-    if (Object.keys(numberOfTimes).some(key => numberOfTimes[key] === 2)) acum[0]++
-    if (Object.keys(numberOfTimes).some(key => numberOfTimes[key] === 3)) acum[1]++
-    
-    return acum
-  }, [ 0, 0 ])
+  const checksum = data.reduce(getValueFromID, [ 0, 0 ])
 
   return checksum[0] * checksum[1]
 }
