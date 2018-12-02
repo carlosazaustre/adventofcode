d98ae658ebed82374b8beb20be28f7b3a9d8ba03
Carlos Azaustre <cazaustre@gmail.com>
2018-12-02 01:40:15 +0100

    refactor

--- day_01.js
+++ day_01.js
@@ -5,24 +5,11 @@ const rl = readline.createInterface({
   input: fs.createReadStream('./day_01_input.txt')
 })
 
-function getSymbol (number) {
-  return number.split('')[0]
-}
-
-function getValue (number) {
-  return parseInt(number.substring(1, number.length))
-}
-
-function isPositive (number) {
-  return (getSymbol(number) === '+') ? true : false
-}
-
 let frequency = 0
-let freqReached = [0]
 
 // Part 1
-rl.on('line', number => { 
-  isPositive(number) ? frequency += getValue(number) : frequency -= getValue(number)
+rl.on('line', num => { 
+  frequency += Number(num)
   console.log(`resulting frequency: ${frequency}`)
 })
 
