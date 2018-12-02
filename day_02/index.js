d87173f089189efd5634adebc3991730ca1639f2
Carlos Azaustre <cazaustre@gmail.com>
2018-12-02 19:04:55 +0100

    refactor day 2 solution using array.reduce

--- day_02/index.js
+++ day_02/index.js
@@ -14,27 +14,38 @@ function getNumberOfTimes (array, value) {
 }
 
 function getChecksum (arrayData) {
-  let twoTimesCounter = 0
-  let threeTimesCounter = 0
-  
-  arrayData.forEach(idString => {
-    const id = idString.split('')
+  // Array Reduce doc:
+  // The reduce() method executes a reducer function 
+  // (that you provide) on each member of the array 
+  // resulting in a single output value.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+  const checksum = arrayData.reduce((acum, current) => {
+    // Convert current string ID into a char array
+    const letters = [...current]
 
-    let twoTimes = false
-    let threeTimes = false
-      
-    id.forEach(letter => {
-      if (getNumberOfTimes(id, letter) === 2) twoTimes = true
-      if (getNumberOfTimes(id, letter) === 3) threeTimes = true
+    let numberOfTimes = {}
+
+    // Store each letter as key in an object.
+    // value will be the number of occurrences/times that
+    // appears.
+    letters.forEach(letter => {
+      numberOfTimes[letter] = numberOfTimes[letter] 
+        ? numberOfTimes[letter]++
+        : 1
     })
+    
+    // Array.prototype.some
+    // The some() method tests whether at least one
+    // element in the array passes the test implemented 
+    // by the provided function.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+    if (Object.keys(numberOfTimes).some(key => numberOfTimes[key] === 2)) acum[0]++
+    if (Object.keys(numberOfTimes).some(key => numberOfTimes[key] === 3)) acum[1]++
+    
+    return acum
+  }, [ 0, 0 ])
 
-    if (twoTimes) twoTimesCounter++
-    if (threeTimes) threeTimesCounter++
-
-    twoTimes = threeTimes = false
-  })
-
-  return twoTimesCounter * threeTimesCounter
+  return checksum[0] * checksum[1]
 }
 
 const arrayIds = getData('day_02/input.txt')
