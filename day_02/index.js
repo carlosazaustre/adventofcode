e11a4dde5b8b6e41f81db79e6d77d1a3bc5d906b
Carlos Azaustre <cazaustre@gmail.com>
2018-12-02 17:23:49 +0100

    refactor day 2 solution

--- day_02/index.js
+++ day_02/index.js
@@ -7,34 +7,35 @@
 
 const { getData } = require('../utils')
 
-const arrayIds = getData('day_02/input.txt')
-
 function getNumberOfTimes (array, value) {
   let times = 0
   array.forEach(v => v === value && times++)
   return times
 }
 
-let twoTimesCounter = 0
-let threeTimesCounter = 0
+function getChecksum (arrayData) {
+  let twoTimesCounter = 0
+  let threeTimesCounter = 0
+  
+  arrayData.forEach(idString => {
+    const id = idString.split('')
 
-arrayIds.forEach(idString => {
-  const id = idString.split('')
+    let twoTimes = false
+    let threeTimes = false
+      
+    id.forEach(letter => {
+      if (getNumberOfTimes(id, letter) === 2) twoTimes = true
+      if (getNumberOfTimes(id, letter) === 3) threeTimes = true
+    })
 
-  let twoTimes = false
-  let threeTimes = false
-    
-  id.forEach(letter => {
-    if (getNumberOfTimes(id, letter) === 2) twoTimes = true
-    if (getNumberOfTimes(id, letter) === 3) threeTimes = true
+    if (twoTimes) twoTimesCounter++
+    if (threeTimes) threeTimesCounter++
+
+    twoTimes = threeTimes = false
   })
-  
-  if (twoTimes) twoTimesCounter++
-  if (threeTimes) threeTimesCounter++
-  
-  twoTimes = threeTimes = false
-})
 
-console.log('twoTimes: ', twoTimesCounter)
-console.log('threeTimes: ', threeTimesCounter)
-console.log('Checksum: ', twoTimesCounter * threeTimesCounter)
+  return twoTimesCounter * threeTimesCounter
+}
+
+const arrayIds = getData('day_02/input.txt')
+console.log(`PART 1 -- List of box IDs Checksum: ${getChecksum(arrayIds)}`)
